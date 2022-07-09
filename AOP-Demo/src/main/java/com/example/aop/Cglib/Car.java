08f8fc132356a8f780d8fd8b749cfeae73d9166a
angrebobo <1172176452@qq.com>
2022-07-09 20:24:33 +0800

    Cglib

---
+++ AOP-Demo/src/main/java/com/example/aop/Cglib/Car.java
@@ -0,0 +1,11 @@
+package com.example.aop.Cglib;
+
+public class Car {
+    public void speed(){
+        System.out.println("当前速度为100迈");
+    }
+
+    public void torque(){
+        System.out.println("当前扭矩为450");
+    }
+}
---
+++ AOP-Demo/src/main/java/com/example/aop/Cglib/CarMethodInterceptor.java
@@ -0,0 +1,37 @@
+package com.example.aop.Cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CarMethodInterceptor implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Object: " + o.getClass());
+        System.out.println("Method: " + method.getName());
+        System.out.println("MethodProxy: " + methodProxy.getClass());
+        if (method.getName().equals("speed"))
+            System.out.println("速度测试开始...");
+        if (method.getName().equals("torque"))
+            System.out.println("扭矩测试开始...");
+
+        Object ret = methodProxy.invokeSuper(o, objects);
+        System.out.println("ret = " + ret);
+        if (method.getName().equals("speed"))
+            System.out.println("速度测试结束！");
+        if (method.getName().equals("torque"))
+            System.out.println("扭矩测试结束！");
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Car.class);
+        enhancer.setCallback(new CarMethodInterceptor());
+        Car car = (Car) enhancer.create();
+        car.speed();
+        car.torque();
+    }
+}
